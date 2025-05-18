8116c93709e8e1f234ad33a94ce26ef3e41db15e
Abdullah Omer Mohammed <mabdul23@depaul.edu>
2025-05-18 16:59:46 -0500

    ContentView Test Push

--- BuckBuddy/BuckBuddy/ContentView.swift
+++ BuckBuddy/BuckBuddy/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "atom")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to React!")
         }
         .padding()
     }
